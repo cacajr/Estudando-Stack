a4daaef2be44601159e02cbb3806aac4c09dc871
CacaJr <espinhado010203@gmail.com>
2020-07-06 18:09:25 -0300

    adding UserController and rash password

--- Backend com Node.js/modulo02/src/routes.js
+++ Backend com Node.js/modulo02/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Carlos Junior',
-        email: 'carlosjr@estudando.com.br',
-        password_hash: '123',
-    });
-
-    return res.json(user);
-})
+routes.post('/users', UserController.store);
 
 export default routes;
