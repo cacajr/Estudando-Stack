e0a91e5d178e43578684ef65ad51de06ed18164a
CacaJr <espinhado010203@gmail.com>
2020-07-06 16:52:10 -0300

    creating connections with DB - class 10

--- Backend com Node.js/modulo02/src/routes.js
+++ Backend com Node.js/modulo02/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello world!!!' })
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Carlos Junior',
+        email: 'carlosjr@estudando.com.br',
+        password_hash: '123',
+    });
+
+    return res.json(user);
 })
 
 export default routes;
