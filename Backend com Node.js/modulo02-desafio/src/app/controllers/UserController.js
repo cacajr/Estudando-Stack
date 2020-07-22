4108a404a13704bf761b759fba53a07407ed0456
CacaJr <espinhado010203@gmail.com>
2020-07-22 16:32:07 -0300

    adding user update

--- Backend com Node.js/modulo02-desafio/src/app/controllers/UserController.js
+++ Backend com Node.js/modulo02-desafio/src/app/controllers/UserController.js
@@ -15,11 +15,31 @@ class UserController {
 
     async update(req, res) {
         //mostrando que o id do usuario veio na requisicao, pois a sessao dele foi
-        //validada pelo middleware auth.js e ele envio esse dado nela atraves do
+        //validada pelo middleware auth.js e ele enviou esse dado nela atraves do
         //userId
-        console.log(req.userId);
+        //console.log(req.userId);
 
-        return res.json({ ok: true });
+        const { email, oldPassword } = req.body;
+
+        const user = await User.findByPk(req.userId);
+
+        if (email != user.email) {
+            const userExists = await User.findOne({ where: { email: req.body.email } });
+
+            if (userExists) {
+                return res.status(400).json({ error: 'User email already exists.' });
+            }
+        }
+
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res.status(401).json({ error: 'Password does not match.' })
+        }
+
+        const { id, name } = await user.update(req.body);
+
+        return res.json({ id, name, email });
+
+        // FALTA TESTAR ESSA REQUISICAO DE UPDATE DE USUARIOS
     }
 }
 
