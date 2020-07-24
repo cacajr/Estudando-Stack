fb8b2ae39b1dbafd1727af3edb28f78b5f15ed7e
CacaJr <espinhado010203@gmail.com>
2020-07-24 19:05:42 -0300

    adding validations to create a recipient

--- Backend com Node.js/modulo02-desafio/src/app/controllers/RecipientController.js
+++ Backend com Node.js/modulo02-desafio/src/app/controllers/RecipientController.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            street: Yup.string().required(),
+            number: Yup.string().required(),
+            complement: Yup.string().required(),
+            state: Yup.string().required(),
+            city: Yup.string().required(),
+            zipcode: Yup.string().required()
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validations fails' });
+        }
+
         const recipientExists = await Recipient.findOne({ where: { name: req.body.name } });
 
         if (recipientExists) {
