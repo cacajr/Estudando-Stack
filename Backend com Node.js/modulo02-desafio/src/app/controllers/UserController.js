cd9c1c719c758224e010c8b2f4cb938f59a1dc7d
CacaJr <espinhado010203@gmail.com>
2020-07-24 17:19:12 -0300

    adding validations

--- Backend com Node.js/modulo02-desafio/src/app/controllers/UserController.js
+++ Backend com Node.js/modulo02-desafio/src/app/controllers/UserController.js
@@ -1,7 +1,18 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().required().min(6),
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validations fails' });
+        }
+
         const userExists = await User.findOne({ where: { email: req.body.email } });
 
         if (userExists) {
@@ -18,6 +29,24 @@ class UserController {
         //validada pelo middleware auth.js e ele enviou esse dado nela atraves do
         //userId
         //console.log(req.userId);
+
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string().email(),
+            oldPassword: Yup.string().min(6),
+            password: Yup.string().min(6)
+                .when('oldPassword', (oldPassword, field) =>
+                    oldPassword ? field.required() : field,
+                ),
+            confirmPassword: Yup.string()
+                .when('password', (password, field) =>
+                    password ? field.required().oneOf([Yup.ref('password')]) : field
+                )
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Validations fails' });
+        }
 
         const { email, oldPassword } = req.body;
 
@@ -38,8 +67,6 @@ class UserController {
         const { id, name } = await user.update(req.body);
 
         return res.json({ id, name, email });
-
-        // FALTA TESTAR ESSA REQUISICAO DE UPDATE DE USUARIOS
     }
 }
 
