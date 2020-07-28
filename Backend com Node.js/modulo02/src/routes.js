87bc1ba90310db43c3b9647e5370c142757ac715
CacaJr <espinhado010203@gmail.com>
2020-07-28 20:26:09 -0300

    adding schedule controller to list appointments to providers

--- Backend com Node.js/modulo02/src/routes.js
+++ Backend com Node.js/modulo02/src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,12 +21,13 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), FileController.store);
-
-routes.post('/appointments', AppointmentController.store);
+routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
 
-routes.get('/providers', ProviderController.index);
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
